f1b2dfa7539418c3932c45f0efc18ce4de020a4b
Annaleah Elizabeth Ernst <haelannaleah@gmail.com>
2016-01-10 16:35:49 +0000

    finalized php tests

--- test_file_decommented.php
+++ test_file_decommented.php
@@ -1,9 +1,10 @@
 <?php
     echo 'This is a test'; 
-       
+    
     echo 'This is yet another ## test';
     echo 'One Final Test'; 
     echo '// is comment? no'
+    echo 'what about /*this*/?'
     
     echo 'this is an ' + 'interrupted line'
 ?>
