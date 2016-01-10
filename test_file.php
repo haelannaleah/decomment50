8b21f39ea93947f893e3a538abee65272971446c
Annaleah Elizabeth Ernst <haelannaleah@gmail.com>
2016-01-10 16:33:49 +0000

    one more comment

--- test_file.php
+++ test_file.php
@@ -2,10 +2,12 @@
     echo 'This is a test'; // This is a one-line c++ style comment
     /* This is a multi line comment
        yet another line of comment */
-       
+    
+    # a single line comment
     echo 'This is yet another ## test';
     echo 'One Final Test'; # This is a one-line shell-style comment
     echo '// is comment? no'
+    echo 'what about /*this*/?'
     // 'is comment?'
     
     /* "what about this?" */
